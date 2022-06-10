5051be3a97e32e68f734d2e30aa97a1f10d7c0a8
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2022-06-10 09:59:47 +0530

    final commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float TurnSpeed = 5;
-    float Speed = 2;
-    [SerializeField] public Animator _animator;
-    [SerializeField] public FixedJoystick _joystick;
+    
+    [SerializeField]  Animator _animator;
+    [SerializeField]  FixedJoystick _joystick;
+    public CinemachineFreeLook camera;
+    public Joystick joystick;
+    bool facingRight = true;
+    public GameObject player;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +21,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        camera.m_XAxis.Value += joystick.Direction.x;
+        camera.m_YAxis.Value -= joystick.Direction.y / 100;
         //float h = Input.GetAxis("Horizontal");
         //float v = Input.GetAxis("Vertical");
-        float h = _joystick.Horizontal;
-        float v = _joystick.Vertical;
-
-        _animator.SetFloat("Speed", v);
-        _animator.SetFloat("TurnSpeed", h);
-
+        var h = _joystick.Horizontal;
+        var v = _joystick.Vertical;
        
+        _animator.SetFloat("TurnSpeed", h);
+        _animator.SetFloat("Speed", v);
+        //player.transform.Rotate(h* v  * Vector3.up );
+
+
     }
     public void OnclickJump()
     {
@@ -33,5 +41,18 @@ public class PlayerMovement : MonoBehaviour
     public void OnclickAttack()
     {
         _animator.SetTrigger("Attack");
+    }
+    public void SeePlayer()
+    {
+        transform.eulerAngles = new Vector3(0, _joystick.Horizontal * 10 + _joystick.Vertical, 0); 
+    }
+
+    public void TurnArround()
+    {
+        
+        facingRight = !facingRight;
+        Vector3 scaleChange = transform.localScale;
+        scaleChange.x *= -1;
+        transform.localScale = scaleChange;
     }
 }
